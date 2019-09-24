5be50442e8ea045fed78f8cb0db8604cfe7b744f
Paulo Correa <pauloyaco@gmail.com>
2019-09-23 23:10:49 -0300

    inclusao de dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { FlightsComponent } from './flights/flights.component';
+import { AirportsComponent } from './airports/airports.component';
+import { AccordionModule } from 'primeng/accordion';   
+import { MenuItem } from 'primeng/api';
+import { TableModule } from 'primeng/table';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';   
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';              
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FlightsComponent,
+    AirportsComponent,
+    AccordionModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    TableModule,
+    PanelModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
