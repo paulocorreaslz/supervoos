ca36a7ef7c231bdecd207c4878c927c9658c8c9f
Paulo Correa <pauloyaco@gmail.com>
2019-09-28 02:28:40 -0300

    server

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/supervoos/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8080);
