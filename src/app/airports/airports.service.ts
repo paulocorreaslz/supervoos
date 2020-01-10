fd9a9de4a2ae325512289005cfcc4c2580eb5692
Paulo Correa <pauloyaco@gmail.com>
2020-01-09 23:55:51 -0300

    inclusao de modelo typescript de aeroporto

--- src/app/airports/airports.service.ts
+++ src/app/airports/airports.service.ts
@@ -9,8 +9,8 @@ export class AirportsService {
 
   apiUrl = 'https://voorepo.herokuapp.com/api/airports';
 
-  //http://localhost:9090/api/airports
-  //https://voorepo.herokuapp.com/api/airports
+  // http://localhost:9090/api/airports
+  // https://voorepo.herokuapp.com/api/airports
   constructor(private httpClient: HttpClient) { }
 
   listar() {
