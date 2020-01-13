414cf34bd3f70f1bcf61853e8a4aee38969193e8
Paulo Correa <pauloyaco@gmail.com>
2020-01-13 00:02:35 -0300

    criacao de auth service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+interface myData {
+  success: boolean,
+  message: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private loggedIn = JSON.parse(localStorage.getItem('logado') || 'false');
+
+  constructor(private http: HttpClient) { }
+
+  setLoggedIn(valor: boolean){
+    this.loggedIn = valor;
+    localStorage.setItem('logado', 'true');
+  }
+
+  get isLoggedIn(){
+    return this.isLoggedIn;
+  }
+
+  getUserPassInformation(username, password){
+    return this.http.post<myData>('/api/auth.php',{
+      username,
+      password
+    })
+
+  }
+
+
 }
